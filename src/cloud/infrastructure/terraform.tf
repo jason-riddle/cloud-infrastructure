12783f3be9e86fbc3f8e76f724a692ab473c2b39
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 10:22:18 -0800

    Add aws section

--- src/cloud/infrastructure/terraform.tf
+++ src/cloud/infrastructure/terraform.tf
@@ -12,6 +12,16 @@ terraform {
   }
 
   required_providers {
+    # DOCS: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+    # ENVS: AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+    # CRED: https://us-east-1.console.aws.amazon.com/iamv2/home?region=us-east-1#/users/details/terraform-cloud-infrastructure?section=permissions
+    # NAME: terraform-cloud-infrastructure
+    # PERM: AdministratorAccess
+    # PREF: AKIAR42
+    aws = {
+      source = "hashicorp/aws"
+    }
+
     # DOCS: https://registry.terraform.io/providers/integrations/github/latest/docs
     # ENVS: GITHUB_TOKEN
     # CRED: https://github.com/settings/tokens
