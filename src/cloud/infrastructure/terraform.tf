a25fb48154304ab17136282444a67792539a7363
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 09:57:10 -0800

    Add tailscale required provider

--- src/cloud/infrastructure/terraform.tf
+++ src/cloud/infrastructure/terraform.tf
@@ -8,4 +8,13 @@ terraform {
       name = "cloud-infrastructure"
     }
   }
+
+  required_providers {
+    # DOCS: https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
+    # ENVS: TAILSCALE_OAUTH_CLIENT_ID, TAILSCALE_OAUTH_CLIENT_SECRET
+    # CRED: https://login.tailscale.com/admin/settings/oauth -> terraform-cloud-infrastructure
+    tailscale = {
+      source = "tailscale/tailscale"
+    }
+  }
 }
