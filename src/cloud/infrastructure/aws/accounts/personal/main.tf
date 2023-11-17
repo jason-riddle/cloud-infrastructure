b97fa5ac52a31bb24ea6a497e6bdb03d383bcbc6
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 14:59:51 -0800

    Add commented out github oidc module

--- src/cloud/infrastructure/aws/accounts/personal/main.tf
+++ src/cloud/infrastructure/aws/accounts/personal/main.tf
@@ -1,1 +1,16 @@
 data "aws_partition" "current" {}
+
+# module "aws_oidc_github" {
+#   source  = "unfunco/oidc-github/aws"
+#   version = "1.7.1"
+#   enabled = true
+
+#   attach_admin_policy     = true
+#   attach_read_only_policy = false
+
+#   iam_role_name = "terraform-cloud-infrastructure-oidc-github"
+
+#   github_repositories = [
+#     "jason-riddle/cloud-infrastructure",
+#   ]
+# }
