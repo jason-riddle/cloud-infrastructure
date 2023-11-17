cfa165498e315745abe3aab19041c28b0f7111fb
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 09:59:55 -0800

    Add prefix

--- src/cloud/infrastructure/terraform.tf
+++ src/cloud/infrastructure/terraform.tf
@@ -1,3 +1,5 @@
+# TF VARS: https://app.terraform.io/app/org-jasonriddle/workspaces/cloud-infrastructure/variables
+
 terraform {
   required_version = ">= 1.0.0"
 
@@ -13,6 +15,7 @@ terraform {
     # DOCS: https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
     # ENVS: TAILSCALE_OAUTH_CLIENT_ID, TAILSCALE_OAUTH_CLIENT_SECRET
     # CRED: https://login.tailscale.com/admin/settings/oauth -> terraform-cloud-infrastructure
+    # PREF: ktxGCS
     tailscale = {
       source = "tailscale/tailscale"
     }
