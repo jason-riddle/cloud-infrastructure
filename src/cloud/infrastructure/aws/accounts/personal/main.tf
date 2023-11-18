c875c06bd5d71ce9daf2b3b4e3c56c313ad70712
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 17:37:48 -0800

    Enable oidc

--- src/cloud/infrastructure/aws/accounts/personal/main.tf
+++ src/cloud/infrastructure/aws/accounts/personal/main.tf
@@ -1,16 +1,16 @@
 data "aws_partition" "current" {}
 
-# module "aws_oidc_github" {
-#   source  = "unfunco/oidc-github/aws"
-#   version = "1.7.1"
-#   enabled = true
+module "aws_oidc_github" {
+  source  = "unfunco/oidc-github/aws"
+  version = "1.7.1"
+  enabled = true
 
-#   attach_admin_policy     = true
-#   attach_read_only_policy = false
+  attach_admin_policy     = true
+  attach_read_only_policy = false
 
-#   iam_role_name = "terraform-cloud-infrastructure-aws-oidc-github"
+  iam_role_name = "terraform-cloud-infrastructure-aws-oidc-github"
 
-#   github_repositories = [
-#     "jason-riddle/cloud-infrastructure",
-#   ]
-# }
+  github_repositories = [
+    "jason-riddle/cloud-infrastructure",
+  ]
+}
