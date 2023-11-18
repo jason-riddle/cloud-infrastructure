85c02a3d8c8e6ab97ed11edd5c3f1d5f863a0c67
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 18:17:48 -0800

    Add tfstate backend

--- src/cloud/infrastructure/aws/accounts/personal/main.tf
+++ src/cloud/infrastructure/aws/accounts/personal/main.tf
@@ -1,1 +1,13 @@
 data "aws_partition" "current" {}
+
+module "tfstate_backend" {
+  source  = "cloudposse/tfstate-backend/aws"
+  version = "1.3.0"
+  enabled = true
+
+  name = "terraform-cloud-infrastructure-tfstate-backend"
+
+  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
+}
