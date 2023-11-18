9adceab6cf2f4c484fd50b0b013ea94b4ee06c52
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-17 17:43:46 -0800

    Output at the root level

--- src/cloud/infrastructure/main.tf
+++ src/cloud/infrastructure/main.tf
@@ -2,6 +2,11 @@ module "aws" {
   source = "./aws/accounts/personal"
 }
 
+output "aws_oidc_github_iam_role_arn" {
+  value     = module.aws.aws_oidc_github_iam_role_arn
+  sensitive = true
+}
+
 module "gh_homelab" {
   source = "./github/repos/homelab"
 }
