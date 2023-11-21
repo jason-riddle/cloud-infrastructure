052b103569f340b8c11af3f565ebb87e21ce602d
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-20 20:10:02 -0800

    Add commented out cognito pool

---
+++ src/cloud/infrastructure/aws/accounts/personal/cognito.tf
@@ -0,0 +1,9 @@
+# resource "aws_cognito_user_pool_client" "client" {
+#   name = "example_client"
+
+#   user_pool_id = aws_cognito_user_pool.pool.id
+# }
+
+# resource "aws_cognito_user_pool" "pool" {
+#   name = "example_pool"
+# }
