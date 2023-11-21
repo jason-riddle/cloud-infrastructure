81527b88571ba5cf9078b50ce65861658e075a2d
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-20 20:00:23 -0800

    Move tfstate to another file

--- src/cloud/infrastructure/aws/accounts/personal/main.tf
+++ src/cloud/infrastructure/aws/accounts/personal/main.tf
@@ -1,17 +1,1 @@
 data "aws_partition" "current" {}
-
-# You cannot create a new backend by simply defining this and then
-# immediately proceeding to "terraform apply". The S3 backend must
-# be bootstrapped according to the simple yet essential procedure in
-# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
-module "tfstate_backend" {
-  source  = "cloudposse/tfstate-backend/aws"
-  version = "1.3.0"
-  enabled = false
-
-  name = "terraform-cloud-infrastructure-tfstate-backend"
-
-  terraform_backend_config_file_path = "."
-  terraform_backend_config_file_name = "backend.tf"
-  force_destroy                      = false
-}
