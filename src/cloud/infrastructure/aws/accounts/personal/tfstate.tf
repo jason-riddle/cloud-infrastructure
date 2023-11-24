98cacbbec97da3b2685f47a38a5102ebbc5ba6ac
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 14:43:17 -0800

    Comment out the state backend

--- src/cloud/infrastructure/aws/accounts/personal/tfstate.tf
+++ src/cloud/infrastructure/aws/accounts/personal/tfstate.tf
@@ -2,14 +2,14 @@
 # immediately proceeding to "terraform apply". The S3 backend must
 # be bootstrapped according to the simple yet essential procedure in
 # https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
-module "tfstate_backend" {
-  source  = "cloudposse/tfstate-backend/aws"
-  version = "1.3.0"
-  enabled = false
+# module "tfstate_backend" {
+#   source  = "cloudposse/tfstate-backend/aws"
+#   version = "1.3.0"
+#   enabled = false
 
-  name = "terraform-cloud-infrastructure-tfstate-backend"
+#   name = "terraform-cloud-infrastructure-tfstate-backend"
 
-  terraform_backend_config_file_path = "."
-  terraform_backend_config_file_name = "backend.tf"
-  force_destroy                      = false
-}
+#   terraform_backend_config_file_path = "."
+#   terraform_backend_config_file_name = "backend.tf"
+#   force_destroy                      = false
+# }
