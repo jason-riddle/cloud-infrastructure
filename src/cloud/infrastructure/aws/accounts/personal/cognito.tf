14f5443de2c230b5c2572835c9ea09d72be39961
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 13:31:36 -0800

    Remove cognito

--- src/cloud/infrastructure/aws/accounts/personal/cognito.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_cognito_user_pool_client" "client" {
-#   name = "example_client"
-
-#   user_pool_id = aws_cognito_user_pool.pool.id
-# }
-
-# resource "aws_cognito_user_pool" "pool" {
-#   name = "example_pool"
-# }
