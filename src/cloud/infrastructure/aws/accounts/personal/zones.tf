0ac357081e8e36f6890fd435852d5f9b8fd8917c
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 14:55:14 -0800

    Create the records

--- src/cloud/infrastructure/aws/accounts/personal/zones.tf
+++ src/cloud/infrastructure/aws/accounts/personal/zones.tf
@@ -5,7 +5,7 @@ module "zones" {
 
   zones = {
     "riddleapps.net" = {
-      comment = "riddleapps.net"
+      comment = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     }
   }
 
@@ -14,23 +14,23 @@ module "zones" {
   }
 }
 
-# module "records" {
-#   source  = "terraform-aws-modules/route53/aws//modules/records"
-#   version = "~> 2.0"
-#   create  = true
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+  create  = true
 
-#   zone_name = keys(module.zones.route53_zone_zone_id)[0]
+  zone_name = keys(module.zones.route53_zone_zone_id)[0]
 
-#   records = [
-#     {
-#       name    = "nextcloud"
-#       type    = "CNAME"
-#       ttl     = 3600
-#       records = [
-#         "nx15310.your-storageshare.de",
-#       ]
-#     },
-#   ]
+  records = [
+    {
+      name    = "nextcloud"
+      type    = "CNAME"
+      ttl     = 3600
+      records = [
+        "nx15310.your-storageshare.de",
+      ]
+    },
+  ]
 
-#   depends_on = [module.zones]
-# }
+  depends_on = [module.zones]
+}
