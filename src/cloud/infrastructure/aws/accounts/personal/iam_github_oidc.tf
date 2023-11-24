2cc58abd4404bb5c5f3cec2d540d0989e5bac573
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 14:40:21 -0800

    Add commented out iam github oidc logic

---
+++ src/cloud/infrastructure/aws/accounts/personal/iam_github_oidc.tf
@@ -0,0 +1,11 @@
+# module "iam_github_oidc_provider" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+#   version = "5.32.0"
+#   create  = true
+# }
+
+# module "iam_github_oidc_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+#   version = "5.32.0"
+#   create  = true
+# }
