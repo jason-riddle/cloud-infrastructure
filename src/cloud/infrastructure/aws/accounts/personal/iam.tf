a0b2bf220c30a6b977f1ee333c50b04ccc057b9b
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 15:50:17 -0800

    Change version

--- src/cloud/infrastructure/aws/accounts/personal/iam.tf
+++ src/cloud/infrastructure/aws/accounts/personal/iam.tf
@@ -1,14 +1,14 @@
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-github-oidc-provider
 # module "iam_github_oidc_provider" {
 #   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-#   version = "5.32.0"
+#   version = "~> 5.0"
 #   create  = true
 # }
 
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-github-oidc-role
 # module "iam_github_oidc_role" {
 #   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
-#   version = "5.32.0"
+#   version = "~> 5.0"
 #   create  = true
 # }
 
