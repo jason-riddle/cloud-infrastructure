853967363535df5b91756f277eee559e0a61103d
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 13:53:52 -0800

    Set the cname record

--- src/cloud/infrastructure/aws/accounts/personal/zone_riddleapps_net.tf
+++ src/cloud/infrastructure/aws/accounts/personal/zone_riddleapps_net.tf
@@ -6,6 +6,14 @@ resource "aws_route53_zone" "riddleapps_net" {
   force_destroy = true
 }
 
+resource "aws_route53_record" "nextcloud_riddleapps_net" {
+  zone_id = aws_route53_zone.jasonriddle_com[*].zone_id
+  name    = "nextcloud"  # Set the subdomain
+  type    = "CNAME"
+  ttl     = 300    # Set the Time to Live (TTL) in seconds
+  records = ["nx15310.your-storageshare.de"]  # Set the target domain or endpoint
+}
+
 output "riddleapps_net_name_servers" {
   value = aws_route53_zone.riddleapps_net[*].name_servers
 }
