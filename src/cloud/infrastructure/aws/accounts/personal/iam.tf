f8a813f6bf51e91c4fbb0c0293cba659fd1b8abf
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 18:10:10 -0800

    Enable the oidc provider

--- src/cloud/infrastructure/aws/accounts/personal/iam.tf
+++ src/cloud/infrastructure/aws/accounts/personal/iam.tf
@@ -1,9 +1,9 @@
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-github-oidc-provider
-# module "iam_github_oidc_provider" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-#   version = "~> 5.0"
-#   create  = true
-# }
+module "iam_github_oidc_provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "~> 5.0"
+  create  = true
+}
 
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-github-oidc-role
 # module "iam_github_oidc_role" {
