c38e258ffa83dba3707136f2e94c1da40a4a730e
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 18:18:21 -0800

    Comment out the role and provider

--- src/cloud/infrastructure/aws/accounts/personal/iam.tf
+++ src/cloud/infrastructure/aws/accounts/personal/iam.tf
@@ -1,24 +1,24 @@
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-github-oidc-provider
-module "iam_github_oidc_provider" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-  version = "~> 5.0"
-  create  = true
-}
+# module "iam_github_oidc_provider" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+#   version = "~> 5.0"
+#   create  = true
+# }
 
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-github-oidc-role
-module "iam_github_oidc_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
-  version = "~> 5.0"
-  create  = false
+# module "iam_github_oidc_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+#   version = "~> 5.0"
+#   create  = false
 
-  subjects = [
-    # You can prepend with `repo:` but it is not required
-    "jason-riddle/cloud-infrastructure"
-    # "repo:jason-riddle/cloud-infrastructure"
-    # "repo:jason-riddle/cloud-infrastructure:pull_request",
-    # "repo:jason-riddle/cloud-infrastructure:ref:refs/heads/main",
-  ]
-}
+#   subjects = [
+#     # You can prepend with `repo:` but it is not required
+#     "jason-riddle/cloud-infrastructure"
+#     # "repo:jason-riddle/cloud-infrastructure"
+#     # "repo:jason-riddle/cloud-infrastructure:pull_request",
+#     # "repo:jason-riddle/cloud-infrastructure:ref:refs/heads/main",
+#   ]
+# }
 
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-user
 module "iam_user" {
