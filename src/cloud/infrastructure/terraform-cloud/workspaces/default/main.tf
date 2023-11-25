b545f2da8bc73ca5a552044854e943d7ba7c2ebc
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-11-24 23:00:01 -0800

    Add the workspace

--- src/cloud/infrastructure/terraform-cloud/workspaces/default/main.tf
+++ src/cloud/infrastructure/terraform-cloud/workspaces/default/main.tf
@@ -3,3 +3,8 @@ data "tfe_ip_ranges" "addresses" {}
 data "tfe_organization" "org" {
   name = "org-jasonriddle"
 }
+
+data "tfe_workspace" "workspace" {
+  name         = "cloud-infrastructure"
+  organization = data.tfe_organization.org.name
+}
