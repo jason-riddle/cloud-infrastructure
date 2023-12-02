81d027bfeff8b4d82855a37f10e8d5a6e3801d73
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-01 22:19:22 -0800

    Add homelab authkey

--- src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -18,3 +18,20 @@ output "github_actions_ansible_role_tailscale_authkey" {
   value     = tailscale_tailnet_key.github_actions_ansible_role_tailscale_authkey[0].key
   sensitive = true
 }
+
+resource "tailscale_tailnet_key" "github_actions_homelab_authkey" {
+  count = 1
+
+  description = "NEW - Homelab Authkey"
+
+  reusable      = true
+  ephemeral     = true
+  preauthorized = true
+  expiry        = 7776000 # 90 Days
+  tags          = ["tag:github-actions"]
+}
+
+output "github_actions_homelab_authkey" {
+  value     = tailscale_tailnet_key.github_actions_homelab_authkey[0].key
+  sensitive = true
+}
