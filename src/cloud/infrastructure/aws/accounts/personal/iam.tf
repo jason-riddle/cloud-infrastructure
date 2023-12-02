2f38b4f61dec1ddb1d117c32722ef1a54db083f3
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-01 22:02:01 -0800

    Set iam keys

--- src/cloud/infrastructure/aws/accounts/personal/iam.tf
+++ src/cloud/infrastructure/aws/accounts/personal/iam.tf
@@ -118,12 +118,22 @@ module "iam_user" {
 
   name = "jason-riddle"
 
-  create_iam_access_key         = false
+  create_iam_access_key         = true
   create_iam_user_login_profile = false
 
   policy_arns = [
     "arn:aws:iam::aws:policy/AdministratorAccess"
   ]
+}
+
+output "iam_user_access_key_id" {
+  value     = module.iam_user.iam_access_key_id
+  sensitive = true
+}
+
+output "iam_user_access_key_secret" {
+  value     = module.iam_user.iam_access_key_secret
+  sensitive = true
 }
 
 # https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-user
