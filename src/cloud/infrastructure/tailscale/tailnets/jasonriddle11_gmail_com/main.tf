4220b855ea0619e8fdbe1cbd3e901c71bea10b1c
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-01 22:14:13 -0800

    Add gha ansible role tailscale authkey

--- src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -1,1 +1,20 @@
 data "tailscale_devices" "devices" {}
+
+# Ephemeral
+
+resource "tailscale_tailnet_key" "github_actions_ansible_role_tailscale_authkey" {
+  count = 1
+
+  description = "NEW - Ansible Role Tailscale Authkey"
+
+  reusable      = true
+  ephemeral     = true
+  preauthorized = true
+  expiry        = 7776000 # 90 Days
+  tags          = ["tag:github-actions"]
+}
+
+output "github_actions_ansible_role_tailscale_authkey" {
+  value     = tailscale_tailnet_key.github_actions_ansible_role_tailscale_authkey[0].key
+  sensitive = true
+}
