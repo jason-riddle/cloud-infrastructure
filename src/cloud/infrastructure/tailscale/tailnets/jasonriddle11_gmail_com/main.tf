66e97a3e47575036c7d2a5fce95178aa28189ca2
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-02 17:00:35 -0800

    Add homelab key

--- src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -39,3 +39,22 @@ output "github_actions_homelab_authkey" {
   value     = tailscale_tailnet_key.github_actions_homelab_authkey[0].key
   sensitive = true
 }
+
+## Homelab
+
+resource "tailscale_tailnet_key" "homelab_authkey" {
+  count = 1
+
+  description = "${local.timestamp} PROD Homelab Authkey"
+
+  reusable      = true
+  ephemeral     = false
+  preauthorized = true
+  expiry        = 7776000 # 90 Days
+  tags          = ["tag:homelab", "tag:prod"]
+}
+
+output "homelab_authkey" {
+  value     = tailscale_tailnet_key.homelab_authkey[0].key
+  sensitive = true
+}
