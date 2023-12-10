c4942fbe728672d39ca624117c59ae92457c6b9f
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-09 21:00:55 -0800

    Update the key name

--- src/cloud/infrastructure/aws/accounts/personal/lightsail.tf
+++ src/cloud/infrastructure/aws/accounts/personal/lightsail.tf
@@ -6,10 +6,10 @@
 #   blueprint_id      = "ubuntu_22_04"
 #   bundle_id         = "medium_3_0"
 
-#   key_pair_name = "jason-riddle"
+#   key_pair_name = aws_lightsail_key_pair.jasons_mac_mini.id
 # }
 
-# resource "aws_lightsail_key_pair" "key_pair" {
-#   name       = "jason-riddle"
+# resource "aws_lightsail_key_pair" "jasons_mac_mini" {
+#   name       = "jason@Jasons-Mac-Mini"
 #   public_key = file("~/.ssh/id_ed25519.pub")
 # }
