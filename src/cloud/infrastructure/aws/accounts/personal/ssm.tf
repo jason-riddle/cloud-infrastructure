8819da01bc94078a76142a7ce3767e94b4781718
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-18 21:50:17 -0800

    Add expiration_date

--- src/cloud/infrastructure/aws/accounts/personal/ssm.tf
+++ src/cloud/infrastructure/aws/accounts/personal/ssm.tf
@@ -6,6 +6,8 @@ resource "aws_ssm_activation" "activation" {
   name        = "test_ssm_activation"
   description = "Test"
 
+  expiration_date = timeadd(timestamp(), "24h")
+
   registration_limit = "1000"
 
   iam_role = data.aws_iam_role.ec2_run_command_role.id
