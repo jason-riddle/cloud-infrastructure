e7ef6cc543a36e5c1bd1fcc510478b6dbc8ea5da
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-18 21:59:37 -0800

    Rename

--- src/cloud/infrastructure/aws/accounts/personal/ssm.tf
+++ src/cloud/infrastructure/aws/accounts/personal/ssm.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "ssm" {
   assume_role_policy = data.aws_iam_policy_document.ssm_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "attachment" {
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core_policy_attachment" {
   role       = aws_iam_role.ssm.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
