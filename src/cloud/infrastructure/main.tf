2066010cef1b2004330b02d1cd2775cbb4845ef8
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-12-20 19:47:03 -0800

    Remove the iam users

--- src/cloud/infrastructure/main.tf
+++ src/cloud/infrastructure/main.tf
@@ -6,15 +6,15 @@ module "aws" {
 
 ### AWS - IAM CLI User
 
-output "iam_cli_user_access_key_id" {
-  value     = module.aws.iam_cli_user_access_key_id
-  sensitive = true
-}
+# output "iam_cli_user_access_key_id" {
+#   value     = module.aws.iam_cli_user_access_key_id
+#   sensitive = true
+# }
 
-output "iam_cli_user_access_key_secret" {
-  value     = module.aws.iam_cli_user_access_key_secret
-  sensitive = true
-}
+# output "iam_cli_user_access_key_secret" {
+#   value     = module.aws.iam_cli_user_access_key_secret
+#   sensitive = true
+# }
 
 ### AWS - IAM System Users
 
