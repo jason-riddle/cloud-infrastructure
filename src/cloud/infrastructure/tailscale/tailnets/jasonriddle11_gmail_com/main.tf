2bf7280d3f17b45138afc965f8b761a4729e5654
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2024-02-19 10:30:44 -0800

    Remove creating ts auth keys

--- src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -6,58 +6,58 @@ data "tailscale_devices" "devices" {}
 
 ## GitHub Actions
 
-resource "tailscale_tailnet_key" "github_actions_ansible_role_tailscale_authkey" {
-  count = 1
+# resource "tailscale_tailnet_key" "github_actions_ansible_role_tailscale_authkey" {
+#   count = 1
 
-  # description = "${local.timestamp} -GHA- Ansible Role Tailscale Authkey"
-  description = "-GHA- Ansible Role Tailscale Authkey"
+#   # description = "${local.timestamp} -GHA- Ansible Role Tailscale Authkey"
+#   description = "-GHA- Ansible Role Tailscale Authkey"
 
-  reusable      = true
-  ephemeral     = true
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:github-actions"]
-}
+#   reusable      = true
+#   ephemeral     = true
+#   preauthorized = true
+#   expiry        = 7776000 # 90 Days
+#   tags          = ["tag:github-actions"]
+# }
 
-output "github_actions_ansible_role_tailscale_authkey" {
-  value     = tailscale_tailnet_key.github_actions_ansible_role_tailscale_authkey[0].key
-  sensitive = true
-}
+# output "github_actions_ansible_role_tailscale_authkey" {
+#   value     = tailscale_tailnet_key.github_actions_ansible_role_tailscale_authkey[0].key
+#   sensitive = true
+# }
 
-resource "tailscale_tailnet_key" "github_actions_homelab_authkey" {
-  count = 1
+# resource "tailscale_tailnet_key" "github_actions_homelab_authkey" {
+#   count = 1
 
-  # description = "${local.timestamp} -GHA- Homelab Authkey"
-  description = "-GHA- Homelab Authkey"
+#   # description = "${local.timestamp} -GHA- Homelab Authkey"
+#   description = "-GHA- Homelab Authkey"
 
-  reusable      = true
-  ephemeral     = true
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:github-actions"]
-}
+#   reusable      = true
+#   ephemeral     = true
+#   preauthorized = true
+#   expiry        = 7776000 # 90 Days
+#   tags          = ["tag:github-actions"]
+# }
 
-output "github_actions_homelab_authkey" {
-  value     = tailscale_tailnet_key.github_actions_homelab_authkey[0].key
-  sensitive = true
-}
+# output "github_actions_homelab_authkey" {
+#   value     = tailscale_tailnet_key.github_actions_homelab_authkey[0].key
+#   sensitive = true
+# }
 
 ## Homelab
 
-resource "tailscale_tailnet_key" "homelab_authkey" {
-  count = 1
+# resource "tailscale_tailnet_key" "homelab_authkey" {
+#   count = 1
 
-  # description = "${local.timestamp} Homelab Authkey"
-  description = "Homelab Authkey"
+#   # description = "${local.timestamp} Homelab Authkey"
+#   description = "Homelab Authkey"
 
-  reusable      = true
-  ephemeral     = false
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:homelab"]
-}
+#   reusable      = true
+#   ephemeral     = false
+#   preauthorized = true
+#   expiry        = 7776000 # 90 Days
+#   tags          = ["tag:homelab"]
+# }
 
-output "homelab_authkey" {
-  value     = tailscale_tailnet_key.homelab_authkey[0].key
-  sensitive = true
-}
+# output "homelab_authkey" {
+#   value     = tailscale_tailnet_key.homelab_authkey[0].key
+#   sensitive = true
+# }
