6e0da71e91c0aa43e2b317be850634225cee5622
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2024-03-16 10:48:47 -0700

    Remove the github actions homelab authkey

--- src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ src/cloud/infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -29,21 +29,26 @@ output "github_actions_ansible_role_tailscale_authkey" {
   sensitive = true
 }
 
-resource "tailscale_tailnet_key" "github_actions_homelab_authkey" {
-  count = 1
+# resource "tailscale_tailnet_key" "github_actions_homelab_authkey" {
+#   count = 1
 
-  # description = "${local.timestamp} -GHA- Homelab Authkey"
-  description = "-GHA- Homelab Authkey"
+#   # description = "${local.timestamp} -GHA- Homelab Authkey"
+#   description = "-GHA- Homelab Authkey"
 
-  reusable      = true
-  ephemeral     = true
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:github-actions"]
-}
+#   reusable      = true
+#   ephemeral     = true
+#   preauthorized = true
+#   expiry        = 7776000 # 90 Days
+#   tags          = ["tag:github-actions"]
+# }
+
+# output "github_actions_homelab_authkey" {
+#   value     = tailscale_tailnet_key.github_actions_homelab_authkey[0].key
+#   sensitive = true
+# }
 
 output "github_actions_homelab_authkey" {
-  value     = tailscale_tailnet_key.github_actions_homelab_authkey[0].key
+  value     = "1"
   sensitive = true
 }
 
